cf4811c926e046b0a6c65a3b4589840fff738f48
LAPTOP-S7BN5M2L\Admin <huukhang3999@gmail.com>
2024-05-07 11:36:01 +0700

    create_project_base

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,10 @@
+const TestComponent = (props) => {
+    return (
+        <div>
+            <h1>This is test component</h1>
+            <h1>This is test component</h1>
+        </div>
+    )
+}
+
+export default TestComponent;
