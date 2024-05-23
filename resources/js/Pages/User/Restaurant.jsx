587c4334ea438fbc4d01650c31120bc4dda9b07a
PhucTran <tranhieuphuc12@gmail.com>
2024-05-23 13:50:14 +0700

    implement menu section in restaurant page

--- resources/js/Pages/User/Restaurant.jsx
+++ resources/js/Pages/User/Restaurant.jsx
@@ -1,11 +1,13 @@
 import Banner from "@/Components/RestaurantPage/Banner";
 import Introduction from "@/Components/RestaurantPage/Introduction";
+import Menu from "@/Components/RestaurantPage/Menu";
 
 const restaurant = (props) => {
     return (
         <>
        <Banner></Banner>
        <Introduction></Introduction>
+       <Menu></Menu>
        </>
     );
 }
